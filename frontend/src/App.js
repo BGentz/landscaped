291e6b21667fd83ce78fb9e7b285f0a635323da8
Bryan Gentz <BryanGentz88@gmail.com>
2020-07-14 14:28:28 -0400

    react redux hooks in progress

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,27 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { simpleAction } from './actions/simpleAction';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useDispatch, useSelector } from "react-redux";
+// Actions
+// import { simpleAction } from './actions/simpleAction';
+// Pages
+// import Home from './pages/Home'
 
-class App extends Component {
-  
-  simpleAction = (event) => {
-    this.props.simpleAction();
-   }
-   
-   render() {
-   return (
-    <div className="App">
-     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
-     </header>
-     <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload
-     </p>
-     <pre>
-      {
-        JSON.stringify(this.props)
-      }
-     </pre>
-     <button onClick={this.simpleAction}>Test redux action</button>
+const simpleAction = () => ({ type: 'SIMPLE_ACTION' })
+
+export const App = () => {
+  const result = useSelector(
+    state => state.result
+    );
+  const dispatch = useDispatch();
+
+
+  return (
+    <div>
+      <pre>
+        { result }
+        </pre>
+      <button onClick={() => dispatch(simpleAction())}>Test redux action</button>
     </div>
-   );
-  }
- }
+  )
+}
 
- const mapStateToProps = state => ({
-  ...state
- })
-
- const mapDispatchToProps = dispatch => ({
-  simpleAction: () => dispatch(simpleAction())
- })
-
- export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App();
